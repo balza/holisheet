d2f9db9a9679d536d01b236d072b6541273a5a6a
balza <balza@hotmail.com>
2016-04-10 17:44:41 +0200

    Using Classes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,63 @@
-import * as mortgage from './mortgage.js';
+class Mortgage {
+
+    constructor(principal, years, rate) {
+        this.principal = principal;
+        this.years = years;
+        this.rate = rate;
+    }
+
+    get monthlyPayment() {
+        let monthlyRate = this.rate / 100 / 12;
+        return this.principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate),
+                    this.years * 12)));
+    }
+
+    get amortization() {
+        let monthlyPayment = this.monthlyPayment;
+        let monthlyRate = this.rate / 100 / 12;
+        let balance = this.principal;
+        let amortization = [];
+        for (let y=0; y<this.years; y++) {
+            let interestY = 0;
+            let principalY = 0;
+            for (let m=0; m<12; m++) {
+                let interestM = balance * monthlyRate;
+                let principalM = monthlyPayment - interestM;
+                interestY = interestY + interestM;
+                principalY = principalY + principalM;
+                balance = balance - principalM;
+            }
+            amortization.push({principalY, interestY, balance});
+        }
+        return amortization;
+    }
+
+}
 
 document.getElementById('calcBtn').addEventListener('click', () => {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-    amortization.forEach(month => console.log(month));
+    let principal = document.getElementById("principal").value;
+    let years = document.getElementById("years").value;
+    let rate = document.getElementById("rate").value;
+    let mortgage = new Mortgage(principal, years, rate);
+    document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (rate / 12).toFixed(2);
+    mortgage.amortization.forEach(month => console.log(month));
     let html = "";
-    amortization.forEach((year, index) => html += `
+    mortgage.amortization.forEach((year, index) => html += `
       <tr>
         <td>${index + 1}</td>
-        <td class="currency">${Math.round(year.principalY)}</td> 
+        <td class="currency">${Math.round(year.principalY)}</td>
         <td class="stretch">
             <div class="flex">
-                <div class="bar principal" 
+                <div class="bar principal"
                      style="flex:${year.principalY};-webkit-flex:${year.principalY}">
                 </div>
-                <div class="bar interest" 
+                <div class="bar interest"
                      style="flex:${year.interestY};-webkit-flex:${year.interestY}">
                 </div>
             </div>
         </td>
-        <td class="currency left">${Math.round(year.interestY)}</td> 
+        <td class="currency left">${Math.round(year.interestY)}</td>
         <td class="currency">${Math.round(year.balance)}</td>
       </tr>
     `);
